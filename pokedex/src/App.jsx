f4702da9981a73ded45e2e0699513c11c6573bb9
Tucano1306 <leonic26@hotmail.com>
2025-02-01 17:25:50 -0500

    fixing issues

--- pokedex/src/App.jsx
+++ pokedex/src/App.jsx
@@ -39,6 +39,7 @@ export default function App() {
             </ProtectedRoute>
           } 
         />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </PokemonProvider>
   );
